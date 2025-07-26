0f0631ea7d47f464a8291afd4ca8e9ebe8cefc47
Sabina Rasheed <sabinarasheed57@gmail.com>
2025-07-26 20:13:59 +0500

    complete color switcher project

---
+++ Day-1/colorSwitcher.js
@@ -0,0 +1,14 @@
+const buttons=document.querySelectorAll('.button');
+const body=document.querySelector('body');
+
+buttons.forEach(button =>{
+  button.addEventListener('click',(e)=>{
+    const color=e.target.id;
+    body.style.backgroundColor=color;
+  })
+})
+
+const resetButton=document.querySelector('.reset');
+resetButton.addEventListener('click',(e)=>{
+  body.style.backgroundColor='white'
+})
