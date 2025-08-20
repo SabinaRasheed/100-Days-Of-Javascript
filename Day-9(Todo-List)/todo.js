eb8c66d0ded3522752930b371c29a90c578286cc
Sabina Rasheed <sabinarasheed57@gmail.com>
2025-08-21 02:05:44 +0500

    Add initial JS logic for todo list app

---
+++ Day-9(Todo-List)/todo.js
@@ -0,0 +1,9 @@
+console.log("Todo List Javascript Loaded")
+
+const addBtn = document.querySelector("#add-todo")
+const addBar = document.querySelector("#addBar")
+const todoList = document.querySelector(".todo-list")
+
+addBtn.addEventListener("click",()=>{
+  console.log("Add Button Clicked")
+})
